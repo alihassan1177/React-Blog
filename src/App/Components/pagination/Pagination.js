ab854c9d0da37b0be6af324bab46d07a70fa96be
Ali-Hassan12 <thealihassan12@gmail.com>
2022-07-06 22:26:25 +0500

    'init'

--- src/App/Components/pagination/Pagination.js
+++ src/App/Components/pagination/Pagination.js
@@ -1,10 +1,6 @@
 import { PureComponent } from "react";
 
 export default class Pagination extends PureComponent {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return <h1>Pagination</h1>;
   }
